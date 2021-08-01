64fee627a8239eab503fdbf79e07c020801fed71
Tom <senteswins@gmail.com>
2021-08-01 10:35:14 +0200

    add styling

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -1,15 +1,27 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/categorie_item.dart';
+import 'package:flutter_complete_guide/dummy_data.dart';
 
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      children: <Widget>[],
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-          maxCrossAxisExtent: 200,
-          crossAxisSpacing: 20,
-          mainAxisSpacing: 20,
-          childAspectRatio: 3 / 2),
-    );
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('DeliMeal'),
+        ),
+        body: GridView(
+          padding: const EdgeInsets.all(25),
+          children: DUMMY_CATEGORIES
+              .map((catData) =>
+                  CategoryItem(title: catData.title, color: catData.color))
+              .toList(),
+          gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+              maxCrossAxisExtent: 200,
+              crossAxisSpacing: 20,
+              mainAxisSpacing: 20,
+              childAspectRatio: 3 / 2),
+        ));
   }
 }
